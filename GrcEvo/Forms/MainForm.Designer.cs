b42de61b69d67557b21674a0f61f3a699d903590
Chiwawaweb <git@chiwawaweb.com>
2018-03-27 23:02:32 +0200

    Maj

---
+++ GrcEvo/Forms/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GrcEvo.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ToolStrip = new System.Windows.Forms.ToolStrip();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // ToolStrip
+            // 
+            this.ToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.ToolStrip.Name = "ToolStrip";
+            this.ToolStrip.Size = new System.Drawing.Size(1071, 25);
+            this.ToolStrip.TabIndex = 1;
+            this.ToolStrip.Text = "toolStrip1";
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Location = new System.Drawing.Point(0, 564);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(1071, 22);
+            this.StatusStrip.TabIndex = 2;
+            this.StatusStrip.Text = "statusStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1071, 586);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.ToolStrip);
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GRC Evo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip ToolStrip;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+    }
+}
--- GrcEvo/Program.cs
+++ GrcEvo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GrcEvo.Forms;
 
 namespace GrcEvo
 {
@@ -16,7 +17,7 @@ namespace GrcEvo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
