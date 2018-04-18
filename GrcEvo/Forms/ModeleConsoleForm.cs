f5b81262fc9287515c024b672bfa5687420ad000
chiwawaweb <git@chiwawaweb.com>
2018-04-18 12:55:37 +0200

    Maj

---
+++ GrcEvo/Forms/ModeleConsoleForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrcEvo.Forms
+{
+    public partial class ModeleConsoleForm : Form
+    {
+        public ModeleConsoleForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
